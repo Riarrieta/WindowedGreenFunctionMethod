0bc10e552da154b337c62622a2430abf3ae4d6c5
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2021-07-08 20:41:46 -0400

    add folder for scripts

--- test/test.f90
+++ test/test.f90
@@ -1,24 +1,51 @@
 program test
-use mpi
-use tools
 implicit none
 
-! MPI variables
-integer :: ierr, N_procs, id
+real(8):: tstart, tfinish
 
-! MPI init
-call mpi_init(ierr) 
-call mpi_comm_size(MPI_COMM_WORLD, N_procs, ierr)
-call mpi_comm_rank(MPI_COMM_WORLD, id, ierr)
-print *, "Total processes:", N_procs, "Process id:", id
-call mpi_barrier(MPI_COMM_WORLD, ierr)
+integer,parameter:: n = 50
+real(8):: A(n,n)
+real(8):: b(n)
 
-if (id == 0) then
-    print *, "HOLAAA"
-end if
+real(8):: a1(n)
+real(8):: a2(n)
+
+integer:: j
 
 
-call MPI_Finalize(ierr)
+call random_number(A)
+call random_number(b)
+
+call cpu_time(tstart)
+a1 = matmul(A, b)
+call cpu_time(tfinish)
+print *, tfinish-tstart
+
+call cpu_time(tstart)
+a2 = blas_matmul(A, b, n, n)
+call cpu_time(tfinish)
+print *, tfinish-tstart
+
+print *, "HOLAAA", .true., all(abs(a1-a2) < 1d-5)
+read(*,*) j
+print *, "caca", j
+
+contains
+function blas_matmul(A, b, m, n) result(y)
+    real(8),intent(in):: A(m,n), b(n)
+    integer,intent(in):: m, n
+    real(8):: y(m)
+    real(8):: alpha, beta
+    integer:: lda, incx, incy
+    alpha = 1d0
+    beta = 0d0
+    lda = m
+    incx = 1
+    incy = 1
+    call dgemv('N', m, n, alpha, A, lda, b, incx, beta, y, incy)
+end function blas_matmul
+
+
 end program
 
 
